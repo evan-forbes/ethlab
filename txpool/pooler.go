577c8aa3995610b3d688d2726eefa9f912850c51
Evan Forbes <evan.samuel.forbes@gmail.com>
2020-04-07 13:31:03 -0500

    changed the way that TxPool schedules the removal of txs that have been replaced.

--- txpool/pooler.go
+++ txpool/pooler.go
@@ -16,21 +16,35 @@ type Pooler interface {
 
 // TxPool maintains a queue of transactions sorted by gas price
 type TxPool struct {
-	Pool map[common.Address]map[uint64]*types.Transaction
-	// SortedTransactions map[]
-	Order []*txID
-	mu    sync.RWMutex
+	Pool         map[common.Address]map[uint64]setTx
+	Order        []*txID
+	mu           sync.RWMutex
+	invalidCount int
+}
+
+type setTx struct {
+	tx   *types.Transaction
+	txID *txID
+}
+
+type txID struct {
+	address  common.Address
+	nonce    uint64
+	gasPrice *big.Int
+	valid    bool
 }
 
 // New inits a new TxPool
 func New() *TxPool {
 	return &TxPool{
-		Pool: make(map[common.Address]map[uint64]*types.Transaction),
+		Pool: make(map[common.Address]map[uint64]setTx),
 	}
 }
 
 // Next pops and returns the highest gas price transaction, along with
-// a bool determining if there are any transactions left.
+// a bool determining if there are any transactions left. nil txs are
+// returned for non-existant and invalid transaction that have yet to
+// be culled.
 func (pool *TxPool) Next() (*types.Transaction, bool) {
 	if len(pool.Order) == 0 {
 		return nil, false
@@ -43,6 +57,10 @@ func (pool *TxPool) Next() (*types.Transaction, bool) {
 	pool.Order[len(pool.Order)-1] = nil
 	pool.Order = pool.Order[:len(pool.Order)-1]
 
+	if !nextID.valid {
+		return nil, true
+	}
+
 	// get the tx from the pool
 	tx, has := pool.Pool[nextID.address][nextID.nonce]
 	if !has {
@@ -53,10 +71,7 @@ func (pool *TxPool) Next() (*types.Transaction, bool) {
 	// remove the transaction from the pool
 	delete(pool.Pool[nextID.address], nextID.nonce)
 
-	// // remove the transaction from the ordered set
-	// remove(pool.Order, nextID, 0, len(pool.Order)-1)
-
-	return tx, true
+	return tx.tx, true
 }
 
 // Insert adds a transaction to the pool, replaceing the old transaction if the nonce is the same.
@@ -71,16 +86,23 @@ func (pool *TxPool) Insert(author common.Address, tx *types.Transaction) {
 	oldtx, has := pool.Pool[author][nonce]
 	if has {
 		// if the gas price is not larger, don't do anything
-		if oldtx.GasPrice().Cmp(tx.GasPrice()) != 1 {
+		if oldtx.tx.GasPrice().Cmp(tx.GasPrice()) != 1 {
 			return
 		}
-		// remove the old transaction from the ordered set before placing
-		// this is the only time the remove func is used and might be uneccessary
-		remove(pool.Order, id, 0, len(pool.Order)-1)
+		// mark the old transaction as invalid
+		oldtx.txID.valid = false
+
+		// count invalid txs to make sure we don't call cull too often
+		pool.invalidCount++
+
+		if pool.invalidCount > 100 {
+			cull(pool.Order)
+			pool.invalidCount = 0
+		}
 	}
 
 	//// add the transaction in the pool ////
-	pool.Pool[author][nonce] = tx
+	pool.Pool[author][nonce] = setTx{tx: tx, txID: id}
 
 	// don't attempt to search and insert the txID if there're none to search
 	if len(pool.Order) == 0 {
@@ -91,12 +113,6 @@ func (pool *TxPool) Insert(author common.Address, tx *types.Transaction) {
 	// put the transaction into the ordered set
 	place(pool.Order, id, 0, len(pool.Order)-1)
 	return
-}
-
-type txID struct {
-	address  common.Address
-	nonce    uint64
-	gasPrice *big.Int
 }
 
 // place inserts a transaction id in order, sorted by gas price
@@ -127,42 +143,12 @@ func place(s []*txID, n *txID, head, tail int) {
 	return
 }
 
-// remove deletes a transaction id from the ordered slice
-// its kind of slow, so try not to use too much.
-func remove(s []*txID, n *txID, head, tail int) {
-	diff := tail - head
-	// if our search has finished
-	if diff == 1 {
-		// check to see if it is the same txID
-		if n.address == s[head].address && n.nonce == s[head].nonce {
-			// remove head
-			sliceDelete(s, head)
-			return
-		}
-		if n.address == s[tail].address && n.nonce == s[tail].nonce {
-			// remove tail
-			sliceDelete(s, tail)
-			return
+func cull(s []*txID) {
+	for i, id := range s {
+		if !id.valid {
+			sliceDelete(s, i)
 		}
 	}
-	delta := head + (diff / 2)
-	node := s[delta]
-	switch node.gasPrice.Cmp(n.gasPrice) {
-	case 0:
-		// check all txs with the potentially same gas price
-		for i := head; i < tail; i++ {
-			if sameTxID(s[i], n) {
-				sliceDelete(s, i)
-			}
-		}
-	case -1:
-		// try again above delta
-		remove(s, n, delta, tail)
-	case 1:
-		// try again below delta
-		remove(s, n, head, delta)
-	}
-	return
 }
 
 func sameTxID(a, b *txID) bool {
@@ -176,3 +162,44 @@ func sliceDelete(a []*txID, i int) {
 	a[len(a)-1] = nil
 	a = a[:len(a)-1]
 }
+
+// RECYCLE
+
+// // remove deletes a transaction id from the ordered slice
+// // its kind of slow, so try not to use too much.
+// func remove(s []*txID, n *txID, head, tail int) {
+// 	diff := tail - head
+// 	// if our search has finished
+// 	if diff == 1 {
+// 		// check to see if it is the same txID
+// 		if n.address == s[head].address && n.nonce == s[head].nonce {
+// 			// remove head
+// 			sliceDelete(s, head)
+// 			return
+// 		}
+// 		if n.address == s[tail].address && n.nonce == s[tail].nonce {
+// 			// remove tail
+// 			sliceDelete(s, tail)
+// 			return
+// 		}
+// 	}
+// 	delta := head + (diff / 2)
+// 	node := s[delta]
+// 	switch node.gasPrice.Cmp(n.gasPrice) {
+// 	case 0:
+// 		// cross our fingers and hope this doesn't happen when the txpool filled with many txs of the same price...
+// 		// check all txs with the potentially same gas price
+// 		for i := head; i < tail; i++ {
+// 			if sameTxID(s[i], n) {
+// 				sliceDelete(s, i)
+// 			}
+// 		}
+// 	case -1:
+// 		// try again above delta
+// 		remove(s, n, delta, tail)
+// 	case 1:
+// 		// try again below delta
+// 		remove(s, n, head, delta)
+// 	}
+// 	return
+// }
