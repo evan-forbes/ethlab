c177638680d0c4ffbaac1e9c69aca49a5a5da913
evan-forbes <evan.samuel.forbes@gmail.com>
2020-06-30 13:13:02 -0500

    docs

--- module/module.go
+++ module/module.go
@@ -12,14 +12,17 @@ import (
 	"github.com/evan-forbes/ethlab/server"
 )
 
+// Deployer wraps around functions intending to deploy a set of smart contracts.
 type Deployer func(u *User) (common.Address, error)
 
+// User represents the data needed to use the ethereuem block chain
 type User struct {
 	Client *ethclient.Client
 	priv   *ecdsa.PrivateKey
 	from   common.Address
 }
 
+// Deploy runs multiple deploy functions using user u's private key
 func (u *User) Deploy(deps ...Deployer) error {
 	for _, dep := range deps {
 		_, err := dep(u)
@@ -44,25 +47,23 @@ func NewUser() (*User, error) {
 	return out, nil
 }
 
-// StarterKit generates a new account and requests
-// eth to it.
+// StarterKit generates a new account and requests eth to it.
 func StarterKit(host string) (*User, error) {
 	user, err := NewUser()
 	if err != nil {
 		return nil, err
 	}
-
 	client, err := ethclient.Dial(host)
 	if err != nil {
 		return nil, err
 	}
 	user.Client = client
-	server.RequestETH(host, user.from.Hex())
+	server.RequestETH(host, user.from.Hex(), big.NewInt(1))
 	return user, nil
 }
 
-// NewTxOpts issues a new transact opt with sane defaults for
-// user u
+// NewTxOpts issues a new transact opt with sane defaults and signs using User
+// u's private key
 func (u *User) NewTxOpts() *bind.TransactOpts {
 	out := bind.NewKeyedTransactor(u.priv)
 	out.GasLimit = 3000000
